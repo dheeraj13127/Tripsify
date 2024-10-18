9ba69f181648d7e168d046db22a6f1f4552621a5
dheeraj13127 <dheerajkumar13127@gmail.com>
2024-10-18 18:56:11 +1100

    made the basic setup and changes

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        "dark-pink": "#ee327a",
+        "dark-white": "#e3ddd3",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        pacifico: ["Pacifico", "cursive"],
+        roboto: ["Roboto", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
